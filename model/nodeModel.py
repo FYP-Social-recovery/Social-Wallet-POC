99f627d14edb7b0c0783ffb6fd75b1f5d5ebff13
Sandaru <180321j@uom.lk>
2023-01-10 07:26:24 +0530

    changes for working in Ganache

--- model/nodeModel.py
+++ model/nodeModel.py
@@ -6,12 +6,12 @@ from solcx import compile_standard, install_solc
 # publicContractAddress=""
 class NodeModel:
     #node constants 
-    myPrivateAddress="58d0efedba9a8a61b2ac3f188dd079782e07aed904cdbc0e3340e073e85c7655"
-    myPublicAddress="0x20543FD8D854d500121215Abc542531987f6bc2e"
-    myContractAddress="0x0B6c62D789e8aA2f9c707cB935D7c00951472b99"
+    myPrivateAddress="2e4d7319aad09af344608bde9dee0aa5a8243f847dc004545927a3488c0f8338"
+    myPublicAddress="0x305eC56922EDcF716F12C7a5c5961147933C0c41"
+    myContractAddress="0x84928115DcA9D57eE2452570A588DE483C804DA1"
     myABI='[ { "inputs": [ { "internalType": "address", "name": "secretOwner", "type": "address" } ], "name": "acceptInvitation", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [ { "internalType": "string[]", "name": "myShares", "type": "string[]" } ], "name": "addMyShares", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [ { "internalType": "address payable", "name": "shareHolder", "type": "address" } ], "name": "addTemporaryShareHolders", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [], "name": "checkAcceptance", "outputs": [ { "internalType": "bool", "name": "", "type": "bool" } ], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [], "name": "distribute", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [], "name": "getMyState", "outputs": [ { "internalType": "string", "name": "", "type": "string" } ], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [], "name": "makeShareHoldersListToDistribute", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [], "name": "makingHolderRequests", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [ { "internalType": "string", "name": "name", "type": "string" } ], "name": "registerToPublicContract", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [ { "internalType": "address", "name": "secretOwner", "type": "address" } ], "name": "rejectInvitation", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [ { "internalType": "address", "name": "secretOwnerAddress", "type": "address" } ], "name": "releaseSecret", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [ { "internalType": "uint256", "name": "index", "type": "uint256" } ], "name": "remove", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [ { "internalType": "address payable", "name": "shareHolder", "type": "address" } ], "name": "removeShareHolders", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [ { "internalType": "string", "name": "name", "type": "string" } ], "name": "requestSharesFromHolders", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [ { "internalType": "string", "name": "share", "type": "string" } ], "name": "saveToRegeneratedShares", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [ { "internalType": "address", "name": "ownerAddress", "type": "address" }, { "internalType": "string", "name": "sharedString", "type": "string" } ], "name": "takeTheSecretFromTheOwner", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [], "stateMutability": "nonpayable", "type": "constructor" }, { "inputs": [], "name": "checkRequestsForBeAHolder", "outputs": [ { "internalType": "address[]", "name": "", "type": "address[]" } ], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "checkRequestsForShare", "outputs": [ { "internalType": "address[]", "name": "", "type": "address[]" } ], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "getMyShares", "outputs": [ { "internalType": "string[]", "name": "", "type": "string[]" } ], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "getShareHolders", "outputs": [ { "internalType": "address[]", "name": "", "type": "address[]" } ], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "isRegistered", "outputs": [ { "internalType": "bool", "name": "", "type": "bool" } ], "stateMutability": "view", "type": "function" }, { "inputs": [ { "internalType": "string", "name": "tempUserName", "type": "string" } ], "name": "isUserNameExist", "outputs": [ { "internalType": "bool", "name": "", "type": "bool" } ], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "myContractAddress", "outputs": [ { "internalType": "address", "name": "", "type": "address" } ], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "myState", "outputs": [ { "internalType": "string", "name": "", "type": "string" } ], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "owner", "outputs": [ { "internalType": "address", "name": "", "type": "address" } ], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "regenerate", "outputs": [ { "internalType": "string[]", "name": "", "type": "string[]" } ], "stateMutability": "view", "type": "function" }, { "inputs": [ { "internalType": "uint256", "name": "", "type": "uint256" } ], "name": "regeneratedShares", "outputs": [ { "internalType": "string", "name": "", "type": "string" } ], "stateMutability": "view", "type": "function" }, { "inputs": [ { "internalType": "uint256", "name": "", "type": "uint256" } ], "name": "requestedShareHolders", "outputs": [ { "internalType": "address payable", "name": "", "type": "address" } ], "stateMutability": "view", "type": "function" }, { "inputs": [ { "internalType": "uint256", "name": "", "type": "uint256" } ], "name": "secretOwners", "outputs": [ { "internalType": "address", "name": "", "type": "address" } ], "stateMutability": "view", "type": "function" }, { "inputs": [ { "internalType": "uint256", "name": "", "type": "uint256" } ], "name": "shareHolders", "outputs": [ { "internalType": "address", "name": "", "type": "address" } ], "stateMutability": "view", "type": "function" }, { "inputs": [ { "internalType": "address", "name": "", "type": "address" } ], "name": "shareHoldersMap", "outputs": [ { "internalType": "string", "name": "", "type": "string" } ], "stateMutability": "view", "type": "function" }, { "inputs": [ { "internalType": "uint256", "name": "", "type": "uint256" } ], "name": "shares", "outputs": [ { "internalType": "string", "name": "", "type": "string" } ], "stateMutability": "view", "type": "function" }, { "inputs": [ { "internalType": "address", "name": "", "type": "address" } ], "name": "sharesMap", "outputs": [ { "internalType": "string", "name": "", "type": "string" } ], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "userName", "outputs": [ { "internalType": "string", "name": "", "type": "string" } ], "stateMutability": "view", "type": "function" } ]'
-    #w3 = Web3(Web3.HTTPProvider("http://127.0.0.1:7545"))
-    w3 = Web3(Web3.HTTPProvider("https://eth-goerli.g.alchemy.com/v2/8L-St1WDAiIktazEqEolQfntGghuPR94"))
+    w3 = Web3(Web3.HTTPProvider("http://127.0.0.1:7545"))
+    #w3 = Web3(Web3.HTTPProvider("https://eth-goerli.g.alchemy.com/v2/8L-St1WDAiIktazEqEolQfntGghuPR94"))
     
 
     # deploy the node smart contract
@@ -58,10 +58,10 @@ class NodeModel:
         NodeModel.myABI=abi
 
         # For connecting to ganache
-        #w3 = Web3(Web3.HTTPProvider("http://127.0.0.1:7545"))
-        w3 = Web3(Web3.HTTPProvider("https://eth-goerli.g.alchemy.com/v2/8L-St1WDAiIktazEqEolQfntGghuPR94"))
-        chain_id = 5
-        #chain_id =1337
+        w3 = Web3(Web3.HTTPProvider("http://127.0.0.1:7545"))
+        #w3 = Web3(Web3.HTTPProvider("https://eth-goerli.g.alchemy.com/v2/8L-St1WDAiIktazEqEolQfntGghuPR94"))
+        #chain_id = 5
+        chain_id =1337
         address = publicAddress
         # leaving the private key like this is very insecure if you are working on real world project
         private_key = privateAddress
@@ -74,7 +74,7 @@ class NodeModel:
         # build transaction
         transaction = ContactList.constructor().buildTransaction(
             {
-                #"chainId": chain_id,
+                "chainId": chain_id,
                 "gasPrice": w3.eth.gas_price,
                 "from": address,
                 "nonce": nonce,
@@ -104,7 +104,7 @@ class NodeModel:
         c= NodeModel.connection(NodeModel.myABI,NodeModel.myContractAddress,owner_addr,private_addr)
         print(owner_addr,"checking user name validity for ",NodeModel.myContractAddress)
         nonce = NodeModel.w3.eth.getTransactionCount(owner_addr)
-        result = c.caller({"from": owner_addr, "gasPrice": NodeModel.w3.eth.gas_price, "nonce": nonce}).isUserNameExist(userName)
+        result = c.caller({"from": owner_addr,"nonce":nonce}).isUserNameExist(userName)
         print("UserName  checked")
         print("Value:", result)
         return result
@@ -215,7 +215,7 @@ class NodeModel:
         c= NodeModel.connection(NodeModel.myABI,NodeModel.myContractAddress,owner_addr,private_addr)
         print(owner_addr,"Adding as a share holder  ",share_holder)
         nonce = NodeModel.w3.eth.getTransactionCount(owner_addr)
-        returnVal = c.functions.addTemporaryShareHolders(share_holder).buildTransaction({"from": owner_addr, "gasPrice": NodeModel.w3.eth.gas_price, "nonce": nonce})
+        returnVal = c.functions.addTemporaryShareHolders(share_holder).buildTransaction({"from": owner_addr,"gasPrice": NodeModel.w3.eth.gas_price,"nonce": nonce})
         # Sign the transaction
         sign_store_contact = NodeModel.w3.eth.account.sign_transaction(returnVal, private_key=private_addr)
         # Send the transaction
@@ -225,7 +225,7 @@ class NodeModel:
         # ownerAddressList = returnVal
         status = transaction_receipt["status"]
         #generate a list of tuples
-       
+
         print("Successfully  added",status)
         
         return status
--- setup.py
+++ setup.py
@@ -2,4 +2,3 @@ from setuptools import setup, find_packages
 setup(name = 'nodeModel', packages = find_packages())
 setup(name='nodeController', packages=find_packages())
 setup(name = 'keyGenerationController', packages = find_packages())
-setup(name = 'nodeController', packages = find_packages())
