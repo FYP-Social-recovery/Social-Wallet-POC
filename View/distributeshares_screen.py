465b3dc39ff19c306e75c60a092e50253240c907
Tharinda98 <tharindathamaranga98@gmail.com>
2023-03-23 12:18:45 +0530

    distirbution screen changed to add email and ditribute the shares. omitted otp hash save.

--- View/distributeshares_screen.py
+++ View/distributeshares_screen.py
@@ -30,13 +30,17 @@ from controller.otp_controller import OTPController
 from state import GlobalState
 
 class DistributesharesScreen(UserControl):
+    
     def __init__(self, on_back_click, on_submit_click, page):
         super().__init__()
         self.on_back_click = on_back_click
         self.on_submit_click = on_submit_click
         self.page = page
-        
+        self.email=""
+        self.otpHash=""
         self.txt = ""
+        self.Emailclient=EmailController()
+        self.OTP_client=OTPController()
         
         def on_dialog_result(e: FilePickerResultEvent):
             print("Selected files:", e.files[0].path)
@@ -50,71 +54,85 @@ class DistributesharesScreen(UserControl):
         self.page.overlay.append(self.file_picker)
         self.page.update()
 
-    
+    def on_verify_email_click_fn(self,e):
+        if(self.email.value):
+            #email an OTP to the email address
+            self.email=self.email.value
+            otp,self.otpHash=self.OTP_client.generateOTPHash()
+            self.Emailclient.sendEmail(self.email,otp)
+        else:
+            self.open_err_dlg_email()
+
     
     def on_submit_click_fn(self,e):
         print(self.txt)
         print(self.biometric.value)
         if(self.biometric.value):
-            if(self.email.value):
-                print("Start Enrolling")
-                # Capture Enrolling fingerprint template
-                original_image_path = self.biometric.value #"../data/Original_fp.BMP"
-                original_image_template = FingerPrintController.read_image(original_image_path)
-                
-                # Preprocessing Fingerprint
-                preprocessed_image_output_path = "../data/Preprocessed_fp.jpg"
+            if(self.otp.value):
+                convertToHash=self.OTP_client.convert_Hash(self.otp.value)[1]
+                if (convertToHash==self.otpHash):
+                    print("OTP verification successful")
+                    print("Start Enrolling")
+                    # Capture Enrolling fingerprint template
+                    original_image_path = self.biometric.value #"../data/Original_fp.BMP"
+                    original_image_template = FingerPrintController.read_image(original_image_path)
+                    
+                    # Preprocessing Fingerprint
+                    preprocessed_image_output_path = "../data/Preprocessed_fp.jpg"
 
-                preprocessed_image = FingerPrintController.fingerprint_pipline(original_image_template, save_image=True, save_path=preprocessed_image_output_path)
-                
-                # Extract minutiea
-                print("Start minutiae extraction")
-                good_fp = False
-                
-                good_fp = FingerPrintController.capture_new_fp_xyt(preprocessed_image_output_path)
-                
-                ## If good fp enroll
-                ## else error
-                if not good_fp:
-                    print(APP_RETRY_FP)
+                    preprocessed_image = FingerPrintController.fingerprint_pipline(original_image_template, save_image=True, save_path=preprocessed_image_output_path)
+                    
+                    # Extract minutiea
+                    print("Start minutiae extraction")
+                    good_fp = False
+                    
+                    good_fp = FingerPrintController.capture_new_fp_xyt(preprocessed_image_output_path)
+                    
+                    ## If good fp enroll
+                    ## else error
+                    if not good_fp:
+                        print(APP_RETRY_FP)
+                    else:
+                        print("Start vault generation")
+                        # Generate vault
+                        secret = int.from_bytes(GlobalState.ENTROPHY_VALUE, byteorder='big')
+                        fuzzy_vault = FingerPrintController.enroll_new_fingerprint(FP_TEMP_FOLDER + FP_OUTPUT_NAME + '.xyt', secret)
+                        
+                        print(fuzzy_vault)
+                        print("\n\n")
+                        
+                        fuzzy_vault_bytes_object = SymmetricEncryption.convertStringToBytesObject(fuzzy_vault)
+                        
+                        print(fuzzy_vault_bytes_object)
+                        print("\n\n")
+                        
+                        encrypted_fuzzy_vault,key  = SymmetricEncryption.encrypt_vault_128_bit_key(fuzzy_vault_bytes_object) # encrypted_fuzzy_vault,key,iv = SymmetricEncryption.encrypt_vault_256_bit_key(fuzzy_vault_bytes_object)
+                        encrypted_fuzzy_vault = SymmetricEncryption.convertByteToString(encrypted_fuzzy_vault)
+                        print(encrypted_fuzzy_vault)
+                        print("\n\n")
+                        
+                        combined_key_bytes = key # SymmetricEncryption.concatanate2BytesObject(key,iv)
+                        
+                        combined_key = SymmetricEncryption.convertBytesObjectToInteger(combined_key_bytes)
+
+                        #print(combined_key)
+                        
+
+
+                        VSS_client=VSS_Controller()
+                        shares=VSS_client.get_generated_shares(int(combined_key))
+                        #print(type(otpHash))
+                        #print(type(encrypted_fuzzy_vault))
+                        NodeContractController.addMyShares(shares=[shares[0],shares[1],shares[2]],publicKeyLocal=GlobalState.PUBLIC_KEY,privateKeyLocal=GlobalState.PRIVATE_KEY,nodeContractAddressLocal=GlobalState.NODE_CONTRACT_ADDRESS)
+                        
+                        #change this function to store email
+                        NodeContractController.distribute(publicKeyLocal=GlobalState.PUBLIC_KEY,privateKeyLocal=GlobalState.PRIVATE_KEY,nodeContractAddressLocal=GlobalState.NODE_CONTRACT_ADDRESS,otp=self.email,vault=encrypted_fuzzy_vault)
+                        
+                        self.on_submit_click(self)
                 else:
-                    print("Start vault generation")
-                    # Generate vault
-                    secret = int.from_bytes(GlobalState.ENTROPHY_VALUE, byteorder='big')
-                    fuzzy_vault = FingerPrintController.enroll_new_fingerprint(FP_TEMP_FOLDER + FP_OUTPUT_NAME + '.xyt', secret)
-                    
-                    print(fuzzy_vault)
-                    print("\n\n")
-                    
-                    fuzzy_vault_bytes_object = SymmetricEncryption.convertStringToBytesObject(fuzzy_vault)
-                    
-                    print(fuzzy_vault_bytes_object)
-                    print("\n\n")
-                    
-                    encrypted_fuzzy_vault,key  = SymmetricEncryption.encrypt_vault_128_bit_key(fuzzy_vault_bytes_object) # encrypted_fuzzy_vault,key,iv = SymmetricEncryption.encrypt_vault_256_bit_key(fuzzy_vault_bytes_object)
-                    encrypted_fuzzy_vault = SymmetricEncryption.convertByteToString(encrypted_fuzzy_vault)
-                    print(encrypted_fuzzy_vault)
-                    print("\n\n")
-                    
-                    combined_key_bytes = key # SymmetricEncryption.concatanate2BytesObject(key,iv)
-                    
-                    combined_key = SymmetricEncryption.convertBytesObjectToInteger(combined_key_bytes)
-
-                    print(combined_key)
-                    
-                    Emailclient=EmailController()
-                    VSS_client=VSS_Controller()
-                    OTP_client=OTPController()
-                    otp,otpHash=OTP_client.generateOTPHash()
-                    Emailclient.sendEmail(self.email.value,otp)
-                    shares=VSS_client.get_generated_shares(int(combined_key))
-                    print(type(otpHash))
-                    print(type(encrypted_fuzzy_vault))
-                    NodeContractController.addMyShares(shares=[shares[0],shares[1],shares[2]],publicKeyLocal=GlobalState.PUBLIC_KEY,privateKeyLocal=GlobalState.PRIVATE_KEY,nodeContractAddressLocal=GlobalState.NODE_CONTRACT_ADDRESS)
-                    NodeContractController.distribute(publicKeyLocal=GlobalState.PUBLIC_KEY,privateKeyLocal=GlobalState.PRIVATE_KEY,nodeContractAddressLocal=GlobalState.NODE_CONTRACT_ADDRESS,otp=otpHash,vault=encrypted_fuzzy_vault)
-                    self.on_submit_click(self)
+                    self.open_err_dlg_otp()
             else:
-                self.open_err_dlg_email()
+                self.open_err_dlg_otp()
         else:
             self.open_err_dlg_fp()
     
@@ -130,6 +148,15 @@ class DistributesharesScreen(UserControl):
     err_dlg_email = AlertDialog(
         title=Text("Email field cannot be empty.", text_align=TextAlign.CENTER), on_dismiss=lambda e: print("Dialog dismissed!")
     )
+
+    def open_err_dlg_otp(self):
+        self.page.dialog = self.err_dlg_otp
+        self.err_dlg_fp.open = True
+        self.page.update()
+        
+    err_dlg_otp = AlertDialog(
+        title=Text("OTP verification failed", text_align=TextAlign.CENTER), on_dismiss=lambda e: print("Dialog dismissed!")
+    )
     
     def open_err_dlg_email(self):
         self.page.dialog = self.err_dlg_email
@@ -139,6 +166,9 @@ class DistributesharesScreen(UserControl):
     def build(self):
         self.email=TextField(label="Email Address",
                           hint_text="Please enter a valid email address", color="0xFF000000", width=600)
+        self.otp=TextField(label="Enter the One-Time password",
+                          hint_text="Please enter the one-time password recieved from email", color="0xFF000000", width=600)
+        
         self.biometric= TextField(label="Select a Fingerprint",
                           hint_text="Please select a fingerprint", color="0xFF000000", width=600)
                         
@@ -157,17 +187,26 @@ class DistributesharesScreen(UserControl):
                 Container(
                     height=20,
                 ),
+                self.email,
+                Container(
+                    height=10,
+                ),
+                #sending an OTP email to verify the email
+                ElevatedButton("Send email verification", bgcolor="#2596be",
+                               color="white",on_click=self.on_verify_email_click_fn, width=300,tooltip="Sending an OTP to verify the email"),
+                Container(
+                    height=10,
+                ),
+                self.otp,
+                Container(
+                    height=10,
+                ),
                 ElevatedButton("Choose finger print image...",
                     on_click=lambda _: self.file_picker.pick_files(allow_multiple=False)),
                 # self.biometric,
                 Container(
                     height=10,
                 ),
-                self.email,
-                Container(
-                    height=10,
-                ),
-                
                 ElevatedButton("Distribute", bgcolor="#2596be",
                                color="white",on_click=self.on_submit_click_fn, width=300,tooltip="Distribute Shares"),
             ],
